a6b5aa86f8ee39fc828d67d8a4d618f00f0acb57
loitp <www.muathu@gmail.com>
2023-05-06 16:39:22 +0700

    up

--- app/src/main/java/com/roy/science/activities/settings/UnitActivity.kt
+++ app/src/main/java/com/roy/science/activities/settings/UnitActivity.kt
@@ -10,55 +10,76 @@ import com.roy.science.R
 import com.roy.science.activities.BaseActivity
 import com.roy.science.preferences.TemperatureUnits
 import com.roy.science.preferences.ThemePreference
-import kotlinx.android.synthetic.main.activity_unit.*
+import kotlinx.android.synthetic.main.activity_unit.backBtnUnit
 import kotlinx.android.synthetic.main.activity_unit.celsiusBtn
+import kotlinx.android.synthetic.main.activity_unit.commonTitleBackunit
+import kotlinx.android.synthetic.main.activity_unit.commonTitleBackUnitColor
 import kotlinx.android.synthetic.main.activity_unit.fahrenheitbtn
 import kotlinx.android.synthetic.main.activity_unit.kelvinBtn
+import kotlinx.android.synthetic.main.activity_unit.unitScroll
+import kotlinx.android.synthetic.main.activity_unit.unitTitle
+import kotlinx.android.synthetic.main.activity_unit.unitTitleDownstate
+import kotlinx.android.synthetic.main.activity_unit.viewUnit
 
-
-class UnitActivity : BaseActivity()  {
+class UnitActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
         val themePreference = ThemePreference(this)
         val themePrefValue = themePreference.getValue()
         if (themePrefValue == 100) {
             when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-                Configuration.UI_MODE_NIGHT_NO -> { setTheme(R.style.AppTheme) }
-                Configuration.UI_MODE_NIGHT_YES -> { setTheme(R.style.AppThemeDark) }
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    setTheme(R.style.AppTheme)
+                }
+
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    setTheme(R.style.AppThemeDark)
+                }
             }
         }
-        if (themePrefValue == 0) { setTheme(R.style.AppTheme) }
-        if (themePrefValue == 1) { setTheme(R.style.AppThemeDark) }
+        if (themePrefValue == 0) {
+            setTheme(R.style.AppTheme)
+        }
+        if (themePrefValue == 1) {
+            setTheme(R.style.AppThemeDark)
+        }
 
         setContentView(R.layout.activity_unit) //REMEMBER: Never move any function calls above this
-        view_unit.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        viewUnit.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
         //Title Controller
-        common_title_back_unit_color.visibility = View.INVISIBLE
-        unit_title.visibility = View.INVISIBLE
-        common_title_back_unit.elevation = (resources.getDimension(R.dimen.zero_elevation))
-        unit_scroll.getViewTreeObserver()
+        commonTitleBackUnitColor.visibility = View.INVISIBLE
+        unitTitle.visibility = View.INVISIBLE
+        commonTitleBackunit.elevation = (resources.getDimension(R.dimen.zero_elevation))
+        unitScroll.viewTreeObserver
             .addOnScrollChangedListener(object : ViewTreeObserver.OnScrollChangedListener {
                 var y = 300f
                 override fun onScrollChanged() {
-                    if (unit_scroll.getScrollY() > 150) {
-                        common_title_back_unit_color.visibility = View.VISIBLE
-                        unit_title.visibility = View.VISIBLE
-                        unit_title_downstate.visibility = View.INVISIBLE
-                        common_title_back_unit.elevation = (resources.getDimension(R.dimen.one_elevation))
+                    if (unitScroll.scrollY > 150) {
+                        commonTitleBackUnitColor.visibility = View.VISIBLE
+                        unitTitle.visibility = View.VISIBLE
+                        unitTitleDownstate.visibility = View.INVISIBLE
+                        commonTitleBackunit.elevation =
+                            (resources.getDimension(R.dimen.one_elevation))
                     } else {
-                        common_title_back_unit_color.visibility = View.INVISIBLE
-                        unit_title.visibility = View.INVISIBLE
-                        unit_title_downstate.visibility = View.VISIBLE
-                        common_title_back_unit.elevation = (resources.getDimension(R.dimen.zero_elevation))
+                        commonTitleBackUnitColor.visibility = View.INVISIBLE
+                        unitTitle.visibility = View.INVISIBLE
+                        unitTitleDownstate.visibility = View.VISIBLE
+                        commonTitleBackunit.elevation =
+                            (resources.getDimension(R.dimen.zero_elevation))
                     }
-                    y = unit_scroll.getScrollY().toFloat()
+                    y = unitScroll.scrollY.toFloat()
                 }
             })
 
         tempUnits()
 
-        back_btn_unit.setOnClickListener {
+        backBtnUnit.setOnClickListener {
             this.onBackPressed()
         }
     }
@@ -101,16 +122,21 @@ class UnitActivity : BaseActivity()  {
         }
     }
 
-    override fun onApplySystemInsets(top: Int, bottom: Int, left: Int, right: Int) {
-        val paramsTitle = common_title_back_unit.layoutParams as ViewGroup.LayoutParams
+    override fun onApplySystemInsets(
+        top: Int,
+        bottom: Int,
+        left: Int,
+        right: Int
+    ) {
+        val paramsTitle = commonTitleBackunit.layoutParams as ViewGroup.LayoutParams
         paramsTitle.height = top + resources.getDimensionPixelSize(R.dimen.title_bar)
-        common_title_back_unit.layoutParams = paramsTitle
+        commonTitleBackunit.layoutParams = paramsTitle
 
-        val paramsLin = unit_title_downstate.layoutParams as ViewGroup.MarginLayoutParams
-        paramsLin.topMargin = top + resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(R.dimen.header_down_margin)
-        unit_title_downstate.layoutParams = paramsLin
+        val paramsLin = unitTitleDownstate.layoutParams as ViewGroup.MarginLayoutParams
+        paramsLin.topMargin =
+            top + resources.getDimensionPixelSize(R.dimen.title_bar) + resources.getDimensionPixelSize(
+                R.dimen.header_down_margin
+            )
+        unitTitleDownstate.layoutParams = paramsLin
     }
 }
-
-
-
