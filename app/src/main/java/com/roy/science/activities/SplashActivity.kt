78b35c2cd0ae13ec7597e19f2d6e3e160055bb8d
loitp <www.muathu@gmail.com>
2023-05-06 21:43:21 +0700

    up

--- app/src/main/java/com/roy/science/activities/SplashActivity.kt
+++ app/src/main/java/com/roy/science/activities/SplashActivity.kt
@@ -1,17 +1,22 @@
 package com.roy.science.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
-
     }
 }
