497f631be5f20f2d287f67ff6c352f4e77e616ef
roy <loitp@galaxyjoy.vn>
2024-07-21 13:37:27 +0700

    up

--- app/src/main/java/com/mckimquyen/atomicPeriodicTable/act/MainAct.kt
+++ app/src/main/java/com/mckimquyen/atomicPeriodicTable/act/MainAct.kt
@@ -22,10 +22,15 @@ import android.view.animation.ScaleAnimation
 import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.doOnLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.applovin.mediation.MaxAd
+import com.applovin.mediation.MaxAdListener
+import com.applovin.mediation.MaxError
+import com.applovin.mediation.ads.MaxInterstitialAd
 import com.mckimquyen.atomicPeriodicTable.R
 import com.mckimquyen.atomicPeriodicTable.act.table.DictionaryAct
 import com.mckimquyen.atomicPeriodicTable.adt.ElementAdt
@@ -105,6 +110,8 @@ class MainAct : TableExt(), ElementAdt.OnElementClickListener2 {
     private var mScale = 1f
     private lateinit var mScaleDetector: ScaleGestureDetector
     private lateinit var gestureDetector: GestureDetector
+
+    private var interstitialAd: MaxInterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -238,6 +245,8 @@ class MainAct : TableExt(), ElementAdt.OnElementClickListener2 {
                 }
             }
         })
+
+        createAdInter()
     }
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
@@ -509,10 +518,12 @@ class MainAct : TableExt(), ElementAdt.OnElementClickListener2 {
             btn.isClickable = true
             btn.isFocusable = true
             btn.setOnClickListener {
-                val intent = Intent(this, ElementInfoAct::class.java)
-                val elementSend = ElementSendAndLoad(this)
-                elementSend.setValue(item.element)
-                startActivity(intent)
+                showAd {
+                    val intent = Intent(this, ElementInfoAct::class.java)
+                    val elementSend = ElementSendAndLoad(this)
+                    elementSend.setValue(item.element)
+                    startActivity(intent)
+                }
             }
         }
     }
@@ -680,5 +691,87 @@ class MainAct : TableExt(), ElementAdt.OnElementClickListener2 {
         val searchEmptyImgPrm = emptySearchBox.layoutParams as ViewGroup.MarginLayoutParams
         searchEmptyImgPrm.topMargin = top + (resources.getDimensionPixelSize(R.dimen.title_bar))
         emptySearchBox.layoutParams = searchEmptyImgPrm
+    }
+
+    private fun createAdInter() {
+        val enableAdInter = getString(R.string.EnableAdInter) == "true"
+        if (enableAdInter) {
+            interstitialAd = MaxInterstitialAd(getString(R.string.INTER), this)
+            interstitialAd?.let { ad ->
+                ad.setListener(object : MaxAdListener {
+                    override fun onAdLoaded(p0: MaxAd) {
+//                        logI("onAdLoaded")
+//                        retryAttempt = 0
+                    }
+
+                    override fun onAdDisplayed(p0: MaxAd) {
+//                        logI("onAdDisplayed")
+                    }
+
+                    override fun onAdHidden(p0: MaxAd) {
+//                        logI("onAdHidden")
+                        // Interstitial Ad is hidden. Pre-load the next ad
+                        interstitialAd?.loadAd()
+                    }
+
+                    override fun onAdClicked(p0: MaxAd) {
+//                        logI("onAdClicked")
+                    }
+
+                    override fun onAdLoadFailed(p0: String, p1: MaxError) {
+//                        logI("onAdLoadFailed")
+//                        retryAttempt++
+//                        val delayMillis =
+//                            TimeUnit.SECONDS.toMillis(2.0.pow(min(6, retryAttempt)).toLong())
+//
+//                        Handler(Looper.getMainLooper()).postDelayed(
+//                            {
+//                                interstitialAd?.loadAd()
+//                            }, delayMillis
+//                        )
+                    }
+
+                    override fun onAdDisplayFailed(p0: MaxAd, p1: MaxError) {
+//                        logI("onAdDisplayFailed")
+                        // Interstitial ad failed to display. We recommend loading the next ad.
+                        interstitialAd?.loadAd()
+                    }
+
+                })
+                ad.setRevenueListener {
+//                    logI("onAdDisplayed")
+                }
+
+                // Load the first ad.
+                ad.loadAd()
+            }
+        }
+    }
+
+    private fun showAd(runnable: Runnable? = null) {
+        val enableAdInter = getString(R.string.EnableAdInter) == "true"
+        if (enableAdInter) {
+            if (interstitialAd == null) {
+                runnable?.run()
+            } else {
+                interstitialAd?.let { ad ->
+                    if (ad.isReady) {
+//                        showDialogProgress()
+//                        setDelay(500.getRandomNumber() + 500) {
+//                            hideDialogProgress()
+//                            ad.showAd()
+//                            runnable?.run()
+//                        }
+                        ad.showAd()
+                        runnable?.run()
+                    } else {
+                        runnable?.run()
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(this, "Applovin show ad Inter in debug mode", Toast.LENGTH_SHORT).show()
+            runnable?.run()
+        }
     }
 }
